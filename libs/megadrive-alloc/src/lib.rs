22e2161326ee2e8d38b56004c6357feb6655e9d8
reinvantveer <reinvantveer@gmail.com>
2021-07-14 15:08:49 +0200

    WIP: expose heap module itself instead of global allocator
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- libs/megadrive-alloc/src/lib.rs
+++ libs/megadrive-alloc/src/lib.rs
@@ -4,13 +4,8 @@
 #![feature(alloc_error_handler)]
 #![feature(default_alloc_error_handler)]
 
-mod heap;
+pub mod heap;
 mod hole;
-
-use crate::heap::Heap;
-
-#[global_allocator]
-pub static mut ALLOCATOR: Heap = Heap::empty();
 
 #[alloc_error_handler]
 fn alloc_error_handler(layout: core::alloc::Layout) -> ! {
