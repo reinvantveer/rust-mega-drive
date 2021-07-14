2c0fef05a88f716a5552a36cd646b32c2469a914
reinvantveer <reinvantveer@gmail.com>
2021-07-14 15:08:08 +0200

    WIP: refcell-wrap all heap struct members
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- libs/megadrive-alloc/src/heap.rs
+++ libs/megadrive-alloc/src/heap.rs
@@ -2,7 +2,7 @@
 // https://github.com/phil-opp/linked-list-allocator/blob/v0.3.0/src/lib.rs
 
 use core::mem;
-use core::alloc::{Layout, AllocError, GlobalAlloc};
+use core::alloc::{AllocError, GlobalAlloc, Layout};
 use core::cell::RefCell;
 
 use crate::hole::{Hole, HoleList};
@@ -10,18 +10,18 @@ use megadrive_sys::heap;
 
 /// A fixed size heap backed by a linked list of free memory blocks.
 pub struct Heap {
-    bottom: usize,
-    size: usize,
-    holes: HoleList,
+    bottom: RefCell<usize>,
+    size: RefCell<usize>,
+    holes: RefCell<HoleList>,
 }
 
 impl Heap {
     /// Creates an empty heap. All allocate calls will return `None`.
     pub const fn empty() -> Heap {
         Heap {
-            bottom: 0,
-            size: 0,
-            holes: HoleList::empty(),
+            bottom: RefCell::new(0),
+            size: RefCell::new(0),
+            holes: RefCell::new(HoleList::empty()),
         }
     }
 
@@ -30,16 +30,18 @@ impl Heap {
     // SAFETY:
     // This function must be called at most once and must only be used on an
     // empty heap.
-    pub unsafe fn init(&mut self) {
+    pub unsafe fn init(&self) {
         // Create the raw slice from the magical heap function from megadrive_sys
         let heap_slice = heap();
 
         // Take a raw pointer to the slice as the bottom
-        self.bottom = heap_slice.as_ptr() as usize;
+        let mut bottom = *self.bottom.borrow_mut();
+        bottom = heap_slice.as_ptr() as usize;
 
         // An then the size as the length of the slice
-        self.size = heap_slice.len();
-        self.holes = HoleList::new(self.bottom, self.size);
+        let mut size = *self.size.borrow_mut();
+        size = heap_slice.len();
+        *self.holes.borrow_mut() = HoleList::new(bottom as usize, size as usize);
     }
 
     /// Allocates a chunk of the given size with the given alignment. Returns a pointer to the
@@ -47,7 +49,7 @@ impl Heap {
     /// This function scans the list of free memory blocks and uses the first block that is big
     /// enough. The runtime is in O(n) where n is the number of free blocks, but it should be
     /// reasonably fast for small allocations.
-    pub fn allocate_first_fit(&mut self, layout: Layout) -> Result<*mut u8, AllocError> {
+    pub fn allocate_first_fit(&self, layout: Layout) -> Result<*mut u8, AllocError> {
         let mut size = layout.size();
         if size < HoleList::min_size() {
             size = HoleList::min_size();
@@ -55,7 +57,7 @@ impl Heap {
         let size = align_up(size, mem::align_of::<Hole>());
         let layout = Layout::from_size_align(size, layout.align()).unwrap();
 
-        self.holes.allocate_first_fit(layout)
+        self.holes.borrow_mut().allocate_first_fit(layout)
     }
 
     /// Frees the given allocation. `ptr` must be a pointer returned
@@ -65,7 +67,7 @@ impl Heap {
     /// This function walks the list of free memory blocks and inserts the freed block at the
     /// correct place. If the freed block is adjacent to another free block, the blocks are merged
     /// again. This operation is in `O(n)` since the list needs to be sorted by address.
-    pub unsafe fn deallocate(&mut self, ptr: *mut u8, layout: Layout) {
+    pub unsafe fn deallocate(&self, ptr: *mut u8, layout: Layout) {
         let mut size = layout.size();
         if size < HoleList::min_size() {
             size = HoleList::min_size();
@@ -73,34 +75,55 @@ impl Heap {
         let size = align_up(size, mem::align_of::<Hole>());
         let layout = Layout::from_size_align(size, layout.align()).unwrap();
 
-        self.holes.deallocate(ptr, layout);
+        self.holes.borrow_mut().deallocate(ptr, layout);
     }
 
     /// Returns the bottom address of the heap.
     pub fn bottom(&self) -> usize {
-        self.bottom
+        *self.bottom.borrow_mut()
     }
 
     /// Returns the size of the heap.
     pub fn size(&self) -> usize {
-        self.size
+        *self.size.borrow_mut()
     }
 
     /// Return the top address of the heap
     pub fn top(&self) -> usize {
-        self.bottom + self.size
+        *self.bottom.borrow_mut() + *self.size.borrow_mut()
     }
 
     /// Extends the size of the heap by creating a new hole at the end
     ///
-    /// # Unsafety
+    /// SAFETY:
     ///
     /// The new extended area must be valid
-    pub unsafe fn extend(&mut self, by: usize) {
+    pub unsafe fn extend(&self, by: usize) {
         let top = self.top();
         let layout = Layout::from_size_align(by, 1).unwrap();
-        self.holes.deallocate(top as *mut u8, layout);
-        self.size += by;
+        let mut holes_mut = self.holes.borrow_mut();
+        holes_mut.deallocate(top as *mut u8, layout);
+        *self.size.borrow_mut() += by;
+    }
+}
+
+/// SAFETY:
+/// The Sync implementation block is required by the compiler in order to have a shared internally
+/// mutable Heap.
+///
+/// This is basically a dummy implementation. The heap cannot be safely used across thread
+/// boundaries as it is likely to lead to data races. So: don't use in multi-threaded apps.
+unsafe impl Sync for Heap {}
+
+unsafe impl GlobalAlloc for Heap {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        self.allocate_first_fit(layout)
+            .ok()
+            .map_or(0 as *mut u8, |allocation| allocation)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        self.deallocate(ptr, layout)
     }
 }
 
@@ -113,22 +136,6 @@ pub fn align_down(addr: usize, align: usize) -> usize {
         addr
     } else {
         panic!("`align` must be a power of 2");
-    }
-}
-
-const HEAP: RefCell<Heap> = RefCell::new(Heap::empty());
-
-unsafe impl GlobalAlloc for Heap {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        HEAP.borrow_mut()
-            .allocate_first_fit(layout)
-            .ok()
-            .map_or(0 as *mut u8, |allocation| allocation)
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        HEAP.borrow_mut()
-            .deallocate(ptr, layout)
     }
 }
 
