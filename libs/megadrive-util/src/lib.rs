3c844cc9d24d6a289a9fc585b8b5a3229d29b5d9
reinvantveer <reinvantveer@gmail.com>
2021-07-28 11:47:42 +0200

    add unstable features and comment on why they are necessary
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- libs/megadrive-util/src/lib.rs
+++ libs/megadrive-util/src/lib.rs
@@ -1,4 +1,10 @@
 #![no_std]
 
+// In order to use the PanicInfo message() method
+#![feature(panic_info_message)]
+
+// In order to transmute to `pub args: &'a [ArgumentV1<'a>]`
+#![feature(fmt_internals)]
+
 pub mod rng;
 mod panic;
